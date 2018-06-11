882efbf8c281679f92fc9fe0e5ab0cf1f05d7bcb
DaPikachuOnMeth <hugomrelvasmartins@gmail.com>
2018-06-11 15:51:00 +0100

    Added classes
    Map
    Tiles

---
+++ RougueLike/Tiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RougueLike
+{
+    class Tiles
+    {
+        bool visible = false;
+        public Bag bag = new Bag();
+        public int idTile = 0;
+    }
+}
