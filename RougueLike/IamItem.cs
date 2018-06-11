7e3d6ea00222c27a4c77694f4af84b98ecbbb8d5
DaPikachuOnMeth <hugomrelvasmartins@gmail.com>
2018-06-11 19:06:38 +0100

    Fixed merge

--- RougueLike/IamItem.cs
+++ RougueLike/IamItem.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace RougueLike
 {
-<<<<<<< HEAD
     public interface IamItem:IHaveWeight
-=======
-    public interface IamItem : IHaveWeight
->>>>>>> 0e405b0b6c2f7afa32614d235373ac9d90235ae2
     {
     }
 }
