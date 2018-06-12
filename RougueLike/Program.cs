a1e0eb528e1554ec33d8a7f808dfb432970feca4
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-06-12 15:43:00 +0100

    drawer added

--- RougueLike/Program.cs
+++ RougueLike/Program.cs
@@ -34,13 +34,6 @@ namespace RougueLike
         }
 
 
-        void MainMenu()
-        {
-            Console.WriteLine("/t/t Newbie Rouguelike/n");
-            Console.WriteLine("/t/t New Game");
-            Console.WriteLine("/t/t High Scores");
-            Console.WriteLine("/t/t Credits");
-            Console.WriteLine("/t/t Quit");
-        }
+        
     }
 }
