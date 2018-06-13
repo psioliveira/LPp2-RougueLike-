fd141151347cb212bac16d07fe7b1870f714ca01
pedrooliveira2007 <pedrooliveira2007@hotmail.com>
2018-06-14 00:46:05 +0100

    started menu

--- RougueLikeRPG/RougueLikeRPG/Menu.cs
+++ RougueLikeRPG/RougueLikeRPG/Menu.cs
@@ -8,8 +8,32 @@ namespace RougueLike
 {
     class Menu
     {
-
+        int selection = 0;
         public Menu()
+        {
+            selection = Convert.ToInt32(Console.ReadLine());
+
+            if (selection == 1) { NewGame(); }
+            if (selection == 2) { HighScores(); }
+            if (selection == 3) { Credits(); }
+            if (selection == 4) { QuitGame(); }
+
+
+        }
+
+        public void NewGame()
+        {
+
+        }
+        public void HighScores()
+        {
+
+        }
+        public void Credits()
+        {
+
+        }
+        public void QuitGame()
         {
 
         }
